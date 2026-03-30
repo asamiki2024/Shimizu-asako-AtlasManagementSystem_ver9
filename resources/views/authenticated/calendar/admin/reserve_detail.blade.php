7eaada6c78fdd898060097d1a780655a0a333c90
asako202404 <disney.asako@icloud.com>
2026-03-30 21:47:59 +0900

    ユーザー情報を表示させる　@foreachの中身を変えてみたがエラーのままでした。

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -5,14 +5,14 @@
     <div class="w-50 m-auto h-75">
       <p><span>日</span><span class="ml-3">部</span></p>
       <div class="h-75 border">
-      @foreach($reservePersons as $reservePerson)
+      @foreach($reservePersons->reserveSettings as $reserveSetting)
       <table class="">
         <tr class="text-center">
           <th class="w-25">ID</th>
           <th class="w-25">名前</th>
         </tr>
         <tr class="text-center">
-          <td class="w-25">{{ $reservePerson->id}}</td>
+          <td class="w-25">{{ $reserveSetting->id}}</td>
           <td class="w-25"></td>
         </tr>
       </table>
