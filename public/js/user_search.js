7146cd06e166db68e30da8c07a1d2b5071fecf5e
asako202404 <disney.asako@icloud.com>
2026-06-13 20:05:43 +0900

    レイアウト変更。 掲示板→アコーディオンメニュー完了。掲示板のレイアウト全て完了。→アコーディオンメニューの矢印をactiveクラスで指定。JSとCSSで指定。動きや矢印の大きさ変更。メインカテゴリー、サブカテゴリーに下線、隙間を付ける。サブカテゴリーのみ内側位置調節。

--- public/js/user_search.js
+++ public/js/user_search.js
@@ -18,7 +18,7 @@ $(function () {
       $('.main_categories').click(function () {
         var category_id = $(this).attr('category_id');
         $('.sub_categories[sub_category_id="'+ category_id +'"]').slideToggle();
-        $(this).toggleClass('open');
+        $(this).toggleClass('active');
       });
 
       // [sub_category_id="'+ category_id +'"]文字列の途中に変数を埋め込むための書き方
