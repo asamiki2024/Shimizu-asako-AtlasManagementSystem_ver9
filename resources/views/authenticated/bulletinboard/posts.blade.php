ab9fb1f966214bba054098556fcd85c96b162d74
asako202404 <disney.asako@icloud.com>
2026-06-15 23:16:48 +0900

    レイアウト変更→掲示板右側ボタン群　メインカテゴリーの下線をサブカテゴリーと統一する。サブカテゴリーのボタンの背景色変更。ボタン群の内側枠線削除。外側の枠線はこれから。　サイドバーの色の幅拡大。

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -31,7 +31,7 @@
     @endforeach
   </div>
   <div class="other_area border w-25">
-    <div class="border m-4">
+    <div>
       <div class="post_btn"><a href="{{ route('post.input') }}">投稿</a></div>
       <div class="post_search">
         <input class="post_keyword" type="text" placeholder="キーワードを検索" name="keyword" name="sub_category" form="postSearchRequest">
