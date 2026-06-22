63b6cc425cbc933d8625db1d8f9963491dc52c03
asako202404 <disney.asako@icloud.com>
2026-06-22 16:55:10 +0900

    レイアウト完了。予約ページ→予約確認画面　カレンダーの枠追加、カレンダーの枠の大きさ調節。背景を追加、余白調整。部数と数字のスペース調節。サイドバーの縦幅拡長。 レイアウトが全て完了。確認待ち

--- resources/views/authenticated/calendar/admin/calendar.blade.php
+++ resources/views/authenticated/calendar/admin/calendar.blade.php
@@ -1,8 +1,12 @@
 <x-sidebar>
 <div class="w-75 m-auto">
-  <div class="w-100">
-    <p>{{ $calendar->getTitle() }}</p>
-    <p>{!! $calendar->render() !!}</p>
+  <div class="w-100 vh-100">
+    <div class="calender-box">
+      <p style="display: flex;
+          justify-content: center;  margin-top: 20px;
+      font-size: 15px;">{{ $calendar->getTitle() }}</p>
+      <p>{!! $calendar->render() !!}</p>
+    </div>
   </div>
 </div>
 </x-sidebar>
